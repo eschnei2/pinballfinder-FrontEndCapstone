1764a5d2ff760477fb4addc46b9d5316834b338c
eschnei2 <everettschneider@gmail.com>
2021-08-02 22:50:42 -0500

    finished added pinball game and google map

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,5 +3,7 @@ import React from "react";
 export const Home = () => (
     <>
         <h1>Welcome to PinballFinder!</h1>
+
+        <iframe src="https://plays.org/game/space-adventure-pinball/" id="gameembed" class="iframegame" frameborder="0" width="600" height ="850"></iframe>
     </>
 )
